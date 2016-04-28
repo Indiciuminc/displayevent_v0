26874df725d27a131e94b644715c6222dc3015ac
jameslogan <jay@indiciumgeospatial.com>
2016-04-28 17:43:29 +0000

    Create basic Events page controllers in /app_server/controllers/events.js

--- app_server/controllers/events.js
+++ app_server/controllers/events.js
@@ -1,4 +1,14 @@
-/* GET Home Page */
-module.exports.index = function(req, res) {
-    res.render('index', {title: 'Display Events' });
+/* GET 'Event Info' Page */
+module.exports.eventInfo = function(req, res) {
+    res.render('index', {title: 'Event Title' });
+};
+
+/* GET 'Event Info Reviews' Page */
+module.exports.eventReviews = function(req, res) {
+    res.render('index', {title: 'Event Title: Reviews' });
+};
+
+/* GET 'Add Review' Page */
+module.exports.addReview = function(req, res) {
+    res.render('index', {title: 'Add A Review' });
 };
