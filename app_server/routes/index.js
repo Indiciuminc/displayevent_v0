7664a769a83572490dfd5164c3016bb29d16a037
jameslogan <jay@indiciumgeospatial.com>
2016-04-28 17:20:16 +0000

    Add routes and controller pointers for Users and Events pages in /app_server/routes/index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var ctrlUsers = require('../controllers/users');
+var ctrlEvents = require('../controllers/events');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+
+/* User Pages */
+router.get('/users', ctrlUsers.userLogin);
+router.get('/users', ctrlUsers.userHome);
+router.get('/users/settings', ctrlUsers.userSettings);
+router.get('/users/settings/time', ctrlUsers.userDefaultTime);
+router.get('/users/settings/myevents', ctrlUsers.userEvents);
+router.get('/users/settings/myevents/new', ctrlUsers.userAddEvent);
+
+/* Event Pages */
+router.get('/events/info', ctrlEvents.eventInfo);
+router.get('/events/info/reviews', ctrlEvents.eventReviews);
+router.get('/events/info/reviews/new', ctrlEvents.addReview);
 
 module.exports = router;
