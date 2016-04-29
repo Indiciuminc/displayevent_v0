9ff301eafb77d6a5da24988f748d18c4e7162ec7
jameslogan <jay@indiciumgeospatial.com>
2016-04-29 13:44:45 +0000

    Add mongoose dependencies to package.json; create dbconnect.js model; hook dbconnect into app.js

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,10 @@
 /* GET Home Page */
 module.exports.index = function(req, res) {
-    res.render('index', {title: 'Display Events' });
+    res.render('index', {
+        title: 'Nowmapr - What\'s out there?'
+    });
 };
+
+//nowEvents GET method
+//nowEvents.find
+//res.render('index', {nowEvents: nowEvents});
