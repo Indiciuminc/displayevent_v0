5bbe9a2895fa184aae4d689108d9f55482532dad
jameslogan <jay@indiciumgeospatial.com>
2016-04-29 13:50:57 +0000

    Add MongoDB connection properties to dbconnect.js model

--- app_server/models/dbconnect.js
+++ app_server/models/dbconnect.js
@@ -1,1 +1,10 @@
 var mongoose = require( 'mongoose' );
+
+var mongoURI = 'mongodb://ds023078.mlab.com:23078/core';
+
+var options = {
+  user: 'admin',
+  pass: 'P@ssw0rd'
+};
+
+mongoose.connect(mongoURI, options);
