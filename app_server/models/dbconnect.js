fb375ed2908bd24c349f8fa9f8b882d4efd399a5
jameslogan <jay@indiciumgeospatial.com>
2016-04-29 13:55:15 +0000

    Add connection status logs to dbconnect model

--- app_server/models/dbconnect.js
+++ app_server/models/dbconnect.js
@@ -8,3 +8,15 @@ var options = {
 };
 
 mongoose.connect(mongoURI, options);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + mongoURI);
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
