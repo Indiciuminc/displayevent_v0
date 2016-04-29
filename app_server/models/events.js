5313c4358641e2dd260e96a69cde8e7cfd0b0d4f
jameslogan <jay@indiciumgeospatial.com>
2016-04-29 14:53:24 +0000

    Create eventSchema in /app_server/models/events.js

--- app_server/models/events.js
+++ app_server/models/events.js
@@ -1,1 +1,39 @@
 var mongoose = require( 'mongoose' );
+
+var eventSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: { 
+        type: Date, 
+        "default": Date.now,
+        required: true
+    },
+    sT: {
+        type: String,
+        required: true
+    },
+    eT: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    coords: {
+        type: [Number],
+        index: '2dsphere',
+        required: true
+    },
+    info: String,
+    admis: String,
+    cost: String,
+    fee: String,
+    faceb: String,
+    insta: String,
+    tweet: String,
+    google: String,
+    web: String,
+});
