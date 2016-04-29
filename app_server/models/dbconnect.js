859d607fb6827fede1c3e77697a2415fae5a11c9
jameslogan <jay@indiciumgeospatial.com>
2016-04-29 14:13:25 +0000

    Add listeners and function to gracefully close Mongoose connection

--- app_server/models/dbconnect.js
+++ app_server/models/dbconnect.js
@@ -33,3 +33,30 @@ mongoose.connection.on('error', function (err) {
 mongoose.connection.on('disconnected', function () {
     console.log('Mongoose disconnected');
 });
+
+//Terminate connection properly
+var exterminateCxn = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+//Listeners to call exterminateCxn when the application terminates
+process.once('SIGUSR2', function () {
+    exterminateCxn('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function () {
+    exterminateCxn('app termination', function () {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function () {
+    exterminateCxn('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
