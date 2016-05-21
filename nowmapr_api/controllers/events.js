ece337fad6475da0b52acdc520dfe04b495aa96c
jameslogan <jay@indiciumgeospatial.com>
2016-05-21 18:22:39 +0000

    Add utility function to Events, Reviews API controllers that sends a status code.

--- nowmapr_api/controllers/events.js
+++ nowmapr_api/controllers/events.js
@@ -1,2 +1,10 @@
+/*Utility function accepting response object, status code, and data */
+var sendJsonResponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
 /*Placeholder Create Event controller for API */
-module.exports.eventsCreate = function (req, res) { };
+module.exports.eventsCreate = function (req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
--- nowmapr_api/controllers/reviews.js
+++ nowmapr_api/controllers/reviews.js
@@ -1,2 +1,10 @@
+/*Utility function accepting response object, status code, and data */
+var sendJsonResponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
 /*Placeholder Create Review controller for API */
-module.exports.reviewsCreate = function (req, res) { };
+module.exports.reviewsCreate = function (req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
