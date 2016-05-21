f4aa5241c3aeca16428e5b805412ad7ca9afbd27
jameslogan <jay@indiciumgeospatial.com>
2016-05-21 22:37:51 +0000

    Add function to eventsReadOne method in Events controller to retrieve single event requested

--- nowmapr_api/controllers/events.js
+++ nowmapr_api/controllers/events.js
@@ -1,7 +1,7 @@
 //Give controller access to database connection
 var mongoose = require('mongoose');
 //Bring in Event model (from '/app_server/models/events.js' schema) to interact with Events collection
-var Events = mongoose.model('Event');
+var Evt = mongoose.model('Event');
 
 /*Utility function accepting response object, status code, and data */
 var sendJsonResponse = function(res, status, content) {
@@ -10,15 +10,26 @@ var sendJsonResponse = function(res, status, content) {
 };
 
 /*Placeholder Create Event controller for API */
-module.exports.eventsCreate = function (req, res) { 
+module.exports.eventsCreate = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
 
 /*Placeholder Read One Event controller for API */
-module.exports.eventsReadOne = function (req, res) { };
+module.exports.eventsReadOne = function (req, res) {
+    Evt
+      //Get eventid from the URL request parameter and give it to Mongoose findById method
+      .findById(req.params.eventid)
+      .exec(function(err, event) {
+        sendJsonResponse(res, 200, event);
+      });
+};
 
 /*Placeholder Update One Event controller for API */
-module.exports.eventsUpdateOne = function (req, res) { };
+module.exports.eventsUpdateOne = function (req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
 
 /*Placeholder Delete One Event controller for API */
-module.exports.eventsDeleteOne = function (req, res) { };
+module.exports.eventsDeleteOne = function (req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
--- nowmapr_api/controllers/reviews.js
+++ nowmapr_api/controllers/reviews.js
@@ -1,7 +1,7 @@
 //Give controller access to database connection
 var mongoose = require('mongoose');
 //Bring in Event model (from '/app_server/models/events.js' schema) to interact with Events collection
-var Events = mongoose.model('Event');
+var Evt = mongoose.model('Event');
 
 /*Utility function accepting response object, status code, and data */
 var sendJsonResponse = function(res, status, content) {
