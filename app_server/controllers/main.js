3a4fb6e5c6310e7c3adedd15d6e14241d6bab2bd
jameslogan <jay@indiciumgeospatial.com>
2016-05-24 20:35:48 +0000

    Cleaning up app_server/controllers/main.js

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -36,8 +36,10 @@ module.exports.index = function(req, res) {
         function(err, response, body) {
             var i, data;
             data = body;
-            for (i=0; i<data.length; i++) {
-                data[i].distance = _formatDistance(data[i].distance);
+            if (response.statusCode === 200 && data.length) {
+                for (i=0; i<data.length; i++) {
+                    data[i].distance = _formatDistance(data[i].distance);
+                }
             }
             renderHomepage(req, res, data);
         }
