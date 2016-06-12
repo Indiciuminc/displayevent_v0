79fb8568bcd40520e7fd7d6634bea144b0b58376
jameslogan <jay@indiciumgeospatial.com>
2016-06-12 17:38:32 +0000

    Angular geolocation added to nowmaprApp.js in nowmaprServices Service. NO USER LOCATION POINT ON MAP YET.

--- public/javascripts/nowmaprApp.js
+++ public/javascripts/nowmaprApp.js
@@ -9,47 +9,100 @@ angular.module('nowmaprApp', ['uiGmapgoogle-maps'])
     });
 })
 
-.controller('displayEvents', function($scope, uiGmapGoogleMapApi) {
+.controller('displayEvents', function($scope, uiGmapGoogleMapApi, nowmaprServices) {
+   
     var areaLat = 45.422503;
     var areaLng = -75.691284;
     var areaZoom = 13;
     
-    uiGmapGoogleMapApi.then(function(maps) {
-        maps.visualRefresh = true;
+    $scope.myCurrentLocation = {};
+    
+    nowmaprServices.getCurrentLocation()
+    
+    .then(function(myCurrentLocation){
+        console.log('myCurrentLocation',myCurrentLocation);
+        $scope.myCurrentLocation = myCurrentLocation;
+    })
+    .then(function(){return uiGmapGoogleMapApi})
+    .then(function(maps){
+        console.log('maps',maps);
+        $scope.googlemap = {};
         $scope.map = {
             center: {
                 latitude: areaLat,
                 longitude: areaLng
             },
             zoom: areaZoom,
-            options: {
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                streetViewControl: false,
-                mapTypeControl: false,
-                scaleControl: false,
-                rotateControl: false,
-                zoomControl: true
+            pan: 1,
+            options: nowmaprServices.getMapOptions().mapOptions,
+            control: {},
+            events: {
+                tilesloaded: function(maps, eventName, args) {
+                    console.log('The ' + eventName + 'function fires every time you move or zoom the map.');
+                },
+                dragend: function(maps, eventName, args) {
+                    console.log('The ' + eventName + 'function fires every time you drag the map.');
+                },
+                zoom_changed: function(maps, eventName, args) {
+                    console.log('The ' + eventName + 'function fires every time you zoom.');
+                }
             },
             showTraficLayer: true
         };
-        console.log($scope.map);
-        $scope.isOffline = false;
+        
+        $scope.map.center = $scope.myCurrentLocation;
+        console.log('maps',maps.LatLngBounds);
+        console.log('function i want to run ONCE after initial map load...');
     });
-});
+})
 
-/*
-.factory('myCoordinates', ['$q', '$http', function myCoordinates($q, $http) {
-    var deferred = $q.defer();
-
-    $http.get('http://ip-api.com/json')
-        .success(function(coordinates) {
-            var myCoordinates = {};
-            myCoordinates.lat = coordinates.lat;
-            myCoordinates.lng = coordinates.lon;
-            myCoordinates.city = coordinates.city;
-            deferred.resolve(myCoordinates);
+.service('nowmaprServices', function($http, $q, uiGmapGoogleMapApi) {
+    /* Function to return map options - used in $scope.map in controller */
+    this.getMapOptions = function(){
+        return{
+            mapOptions: {
+                minZoom: 3,
+                zoomControl: true,
+                draggable: true,
+                navigationControl: false,
+                mapTypeControl: false,
+                scaleControl: false,
+                streetViewControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDoubleClickZoom: false,
+                keyboardShortcuts: false,
+                styles: [{
+                    featureType: "poi",
+                    elementType: "labels",
+                    stylers: [{
+                        visibility:"off"
+                    }]
+                }, {
+                    featureType: "transit",
+                    elementType: "all",
+                    stylers: [{
+                        visibility: "off"
+                    }]
+                }],
+            }
+        };
+    };
+    
+    this.getCurrentLocation = function(){
+        var deferred = $q.defer();
+        
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log('position.coords :', position.coords);
+            var myCurrentLocation = {
+                latitude : position.coords.latitude,
+                longitude : position.coords.longitude
+            };
+            
+            deferred.resolve(myCurrentLocation);
         });
         
-    return deferred.promise;
-}]);
-*/
+        return deferred.promise;
+    };
+});
+    
+   
